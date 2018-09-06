20516501cd72162fa2990db852f4287133770caa
r0lodex <rolodex@live.co.uk>
2018-09-06 11:27:02 +0800

    Updated code to support python 2.7

--- db.py
+++ db.py
@@ -5,7 +5,7 @@
 @created: 04-09-2018
 """
 
-import configparser
+import ConfigParser
 import coloredlogs, logging
 import datetime
 import mysql.connector as database
@@ -13,34 +13,34 @@ import mysql.connector as database
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='DEBUG', logger=logger)
 
-config = configparser.ConfigParser()
+config = ConfigParser.ConfigParser()
 
 try:
-    config.read_file(open('config-production.ini'))
+    config.readfp(open('config-production.ini'))
     logging.basicConfig(filename='LogService.log', filemode='w', level=logging.DEBUG)
-except FileNotFoundError:
+except IOError:
     config.read('config.ini')
 
 for c in config.sections():
-    logger.warning('Database: {0}'.format(config[c]['DATABASE']))
+    logger.warning('Database: {0}'.format(config.get(c, 'DATABASE')))
 
 connection = {
-    'ORIGIN': database.connect(user=config['ORIGIN']['USER'],
-                     password=config['ORIGIN']['PASSWORD'],
-                     database=config['ORIGIN']['DATABASE'],
-                     port=config['ORIGIN']['PORT'],
-                     host=config['ORIGIN']['HOST']
+    'ORIGIN': database.connect(user=config.get('ORIGIN', 'USER'),
+                     password=config.get('ORIGIN', 'PASSWORD'),
+                     database=config.get('ORIGIN', 'DATABASE'),
+                     port=config.get('ORIGIN', 'PORT'),
+                     host=config.get('ORIGIN', 'HOST')
                      ),
-    'ARCHIVE': database.connect(user=config['ARCHIVE']['USER'],
-                     password=config['ARCHIVE']['PASSWORD'],
-                     database=config['ARCHIVE']['DATABASE'],
-                     port=config['ARCHIVE']['PORT'],
-                     host=config['ARCHIVE']['HOST']
+    'ARCHIVE': database.connect(user=config.get('ARCHIVE', 'USER'),
+                     password=config.get('ARCHIVE', 'PASSWORD'),
+                     database=config.get('ARCHIVE', 'DATABASE'),
+                     port=config.get('ARCHIVE', 'PORT'),
+                     host=config.get('ARCHIVE', 'HOST')
                      )
 }
 
 ORIGIN = connection['ORIGIN'].cursor(buffered=True)
 ARCHIVE = connection['ARCHIVE'].cursor(buffered=True)
-ORIGIN_TABLE = config['ORIGIN']['TABLE']
-ARCHIVE_TABLE = config['ARCHIVE']['TABLE']
-TIMEDELTA = config['STATE']['SYNC_DELTA_DAYS']
+ORIGIN_TABLE = config.get('ORIGIN', 'TABLE')
+ARCHIVE_TABLE = config.get('ARCHIVE', 'TABLE')
+TIMEDELTA = config.get('STATE', 'SYNC_DELTA_DAYS')
